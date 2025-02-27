e6757c6376876a6c01d7a38f4f1e0974b0593de6
Filipe Lima <filipe.lima.terceiros2@claro.com.br>
2025-02-26 21:12:18 -0300

    ajustes nos parâmetros do test

--- application-web/test/app.test.js
+++ application-web/test/app.test.js
@@ -1,19 +1,19 @@
-const request = require('supertest');
-const { app } = require('../app/server');
-
-describe('Testes da aplicação web', () => {
-    it('Deve responder com status 200 na rota principal', async () => {
-        const response = await request(app).get('/');
-        expect(response.status).toBe(200);
-        expect(response.text).toBe('Aplicação web rodando!');
-    });
-});
-
-
 //const request = require('supertest');
-//const app = require('../app/server'); // Corrija esse caminho conforme a estrutura do projeto
+//const { app } = require('../app/server');
 //
-//test('Deve responder com uma mensagem', async () => {
-//    const response = await request(app).get('/');
-//    expect(response.text).toBe('Aplicação web rodando!');
+//describe('Testes da aplicação web', () => {
+//    it('Deve responder com status 200 na rota principal', async () => {
+//        const response = await request(app).get('/');
+//        expect(response.status).toBe(200);
+//        expect(response.text).toBe('Aplicação web rodando!');
+//    });
 //});
+
+
+const request = require('supertest');
+const app = require('../app/server'); // Corrija esse caminho conforme a estrutura do projeto
+
+test('Deve responder com uma mensagem', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Aplicação web rodando!');
+});
