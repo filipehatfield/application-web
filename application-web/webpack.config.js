919aeca9ee8517e51d4173ada0b123739ac0368a
Filipe Lima <filipe.lima.terceiros2@claro.com.br>
2025-02-26 22:17:21 -0300

    ajustando o step do sonarscanner

--- application-web/webpack.config.js
+++ application-web/webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app/server.js',
+  entry: './app/server.js', // Ajuste para seu arquivo de entrada
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production',
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "url": require.resolve("url/")
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
