fc56fc4848e686a485cf085bc43ff06d7fdf2b05
Filipe Lima <filipe.lima.terceiros2@claro.com.br>
2025-02-26 21:10:51 -0300

    ajustes nos parâmetros do test

--- application-web/test/app.test.js
+++ application-web/test/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app } = require('../app');
+const { app } = require('../app/server');
 
 describe('Testes da aplicação web', () => {
     it('Deve responder com status 200 na rota principal', async () => {
@@ -8,3 +8,12 @@ describe('Testes da aplicação web', () => {
         expect(response.text).toBe('Aplicação web rodando!');
     });
 });
+
+
+//const request = require('supertest');
+//const app = require('../app/server'); // Corrija esse caminho conforme a estrutura do projeto
+//
+//test('Deve responder com uma mensagem', async () => {
+//    const response = await request(app).get('/');
+//    expect(response.text).toBe('Aplicação web rodando!');
+//});
